afea56d431ce9cc918fbdbf29ae75979fae5272f
zhangweiii <zhangwei.ink@gmail.com>
2019-11-06 23:29:46 +0800

    feat: 🎸 docker

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM python:3.6
 WORKDIR /Project/idiom
 
 COPY requirements.txt ./
-RUN pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+RUN pip install -r requirements.txt -i https://pypi.douban.com/simple
 
 COPY . .
 
-CMD ["gunicorn", "start:app", "-c", "./gunicorn.conf.py"]
+# CMD ["gunicorn", "start:app", "-c", "./gunicorn.conf.py"]
+CMD ["flask", "run", "-h", "0.0.0.0"]
--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from game import Game
 
 app = Flask(__name__)
 
 
-@app.route('/<string:key>')
-def index(key):
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/idiom/<string:key>')
+def idiom(key):
     game = Game(key)
     return jsonify({"data": game.start()})
+
+
+if __name__ == '__main__':
+    app.run()
